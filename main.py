13c9269e57923805e85fded79beb8761461bc611
pug stonks <thatonepuggo@gmail.com>
2024-02-14 17:28:24 -0500

    radio 2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 from discord import FFmpegPCMAudio, PCMVolumeTransformer
 from discord.ext import commands
-from discord.ext.commands import Cog
+from discord.ext.commands import Command
 from discord.ext.commands import Context
-from discord import Interaction, app_commands
+from discord.colour import Color
 import discord
 from typing import Literal, Optional
 
@@ -19,9 +19,13 @@ intents.presences = False
 
 client = commands.Bot(command_prefix='+', intents=intents)
 
-FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+channels = {}
 
-# https://about.abstractumbra.dev/discord.py/2023/01/29/sync-command-example.html
+# https://stackoverflow.com/questions/4092528/how-can-i-clamp-clip-restrict-a-number-to-some-range
+def clamp(n, smallest, largest): return max(smallest, min(n, largest))
+
+
 def search_one(query):
     search_results = api.search(query)['hits']['hits']
     highest_score = 0
@@ -39,55 +43,108 @@ def search_one(query):
                 pick_id = api.get_id(source['url'])
     return source, pick_id
 
+def perm_check(ctx: Context, runner):
+    return runner == ctx.author.id and ctx.channel.permissions_for(ctx.author).manage_channels
 
-@client.command(description='Syncs bot commands.')
-@commands.guild_only()
-@commands.is_owner()
-async def sync(ctx: Context, guilds: commands.Greedy[discord.Object],
-               spec: Optional[Literal["~", "*", "^"]] = None) -> None:
-    if not guilds:
-        if spec == "~":
-            synced = await ctx.bot.tree.sync(guild=ctx.guild)
-        elif spec == "*":
-            ctx.bot.tree.copy_global_to(guild=ctx.guild)
-            synced = await ctx.bot.tree.sync(guild=ctx.guild)
-        elif spec == "^":
-            ctx.bot.tree.clear_commands(guild=ctx.guild)
-            await ctx.bot.tree.sync(guild=ctx.guild)
-            synced = []
-        else:
-            synced = await ctx.bot.tree.sync()
+async def vc_play(ctx: Context, vc: discord.VoiceClient, source, channel_id):
+    vc.play(FFmpegPCMAudio(api.listen_url(channel_id)))
+    await ctx.reply(f"now playing: {source['title']} from station {source['subtitle']} (please wait)")
 
-        await ctx.send(
-            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
-        )
-        return
-
-    ret = 0
-    for guild in guilds:
-        try:
-            await ctx.bot.tree.sync(guild=guild)
-        except discord.HTTPException:
-            pass
-        else:
-            ret += 1
-
-    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
-
-
-@client.command(description="Play a radio station.")
+@client.command(description="play a radio station.")
 async def play(ctx: Context, *, args: str):
     source, channel_id = search_one(args)
     channel = ctx.message.author.voice.channel
+
+    if not channel:
+        await ctx.reply("please join a voice channel")
+        return
     print(source)
-    if channel:
+
+    if channel.id not in channels.keys():
         vc = await channel.connect()
-        vc.play(discord.FFmpegPCMAudio(api.listen_url(channel_id)))
-        await ctx.reply(f"now playing: {source['title']} - {source['subtitle']} (please wait)")
+        channels[channel.id] = {"vc": vc, "author": ctx.author.id, "volume": 1}
+        await vc_play(ctx, vc, source, channel_id)
+    else:
+        vc: discord.VoiceClient = channels[channel.id][0]
+        vc.stop()
+        await vc_play(ctx, vc, source, channel_id)
+
+@client.command(description="stop the radio.")
+async def stop(ctx: Context):
+    channel = ctx.message.author.voice.channel
+
+    if not channel:
+        await ctx.reply("please join a voice channel")
+        return
+
+    if channel.id not in channels.keys():
+        await ctx.reply(f"not in <#{channel.id}>!")
+        return
+
+    vc: discord.VoiceClient = channels[channel.id]["vc"]
+    runner = channels[channel.id]["author"]
+
+    if not perm_check(ctx, runner):
+        await ctx.reply("you do not have permissions to stop the radio.")
+        return
+
+    await vc.disconnect()
+    del channels[channel.id]
+
+
+# @client.command(description="change the volume (0-1).")
+# async def vol(ctx: Context, volume: float):
+#     channel = ctx.message.author.voice.channel
+#
+#     if channel.id not in channels:
+#         await ctx.reply(f"not in <#{channel.id}>!")
+#         return
+#
+#     runner = channels[channel.id]["author"]
+#
+#     if not perm_check(ctx, runner):
+#         await ctx.reply("you do not have permissions to change the volume of the radio.")
+#         return
+#
+#     volume = clamp(volume, 0, 1)
+#     channels[channel.id]["volume"] = volume
+
+
+class MyHelpCommand(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        e = discord.Embed(color=Color.blurple(), description='')
+        for page in self.paginator.pages:
+            e.description += page
+        await destination.send(embed=e)
+
+    @staticmethod
+    def description_append(cmd: Command):
+        if cmd.name == "help":
+            return f'- show this message.'
+        if cmd.description:
+            return f'- {cmd.description}'
+        return ''
+
+    def command_not_found(self, string):
+        return f":x: **Command **'{string}'** not found.**"
+
+    def subcommand_not_found(self, command, string):
+        if isinstance(command, commands.Group) and len(command.all_commands) > 0:
+            return f":x: **Command **'{command.qualified_name}'** has no subcommand named **'{string}'**.**"
+        return f":x: **Command **'{command.qualified_name}'** has no subcommands.**"
+
+    def add_bot_commands_formatting(self, cmds, heading):
+        if cmds:
+            joined = '\n'.join(f'**{c.name}** {self.description_append(c)}' for c in cmds)
+            self.paginator.add_line(f'__**{heading}**__')
+            self.paginator.add_line(joined)
+
+
+client.help_command = MyHelpCommand()
 
 @client.event
 async def on_ready():
-    await client.tree.sync(guild=discord.Object(id=458771009135443998))
     print(f"Logged in as {client.user.name}!")
 
 
